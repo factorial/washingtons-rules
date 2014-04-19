b3c381f5a692ed831e9d6de6626497fa4bc16527
John Hayes <john.hayes@turner.com>
2014-04-19 02:31:53 -0400

    [issue #2] Core data setup & data init code.

--- Washington's Rules/WashingtonsAppDelegate.m
+++ Washington's Rules/WashingtonsAppDelegate.m
@@ -10,8 +10,21 @@
 
 @implementation WashingtonsAppDelegate
 
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    /* To generate new default rules:
+        1. Modify createStarterDataSet: to include your new data
+        2. Uncomment this line
+        3. Run
+        4. Go find the WashingtonsDataModel.sqlite in iPhone Simulator's Documents folder and overwrite WashingtonsDefaultData.sqlite with it
+        5. Comment out this line again
+     */
+    //[self createStarterDataSet];
     
     return YES;
 }
@@ -41,6 +54,125 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)saveContext
+{
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
+}
+
+#pragma mark - Core Data stack
+
+// Returns the managed object context for the application.
+// If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+- (NSManagedObjectContext *)managedObjectContext
+{
+    if (_managedObjectContext != nil) {
+        return _managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        _managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [_managedObjectContext setPersistentStoreCoordinator:coordinator];
+    }
+    return _managedObjectContext;
+}
+
+// Returns the managed object model for the application.
+// If the model doesn't already exist, it is created from the application's model.
+- (NSManagedObjectModel *)managedObjectModel
+{
+    if (_managedObjectModel != nil) {
+        return _managedObjectModel;
+    }
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"WashingtonsDataModel" withExtension:@"momd"];
+    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return _managedObjectModel;
+}
+
+// Returns the persistent store coordinator for the application.
+// If the coordinator doesn't already exist, it is created and the application's store added to it.
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+    @synchronized (self)
+    {
+        if (_persistentStoreCoordinator != nil)
+            return _persistentStoreCoordinator;
+        
+        NSString *defaultStorePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"WashingtonsDefaultData" ofType:@"sqlite"];
+        NSURL *storeDirectoryURL = [self applicationDocumentsDirectory];
+        NSString *storePath = [[storeDirectoryURL relativePath] stringByAppendingPathComponent: @"WashingtonsDataModel.sqlite"];
+        NSURL *storePathURL = [NSURL fileURLWithPath:storePath];
+        
+        NSError *error;
+        if (![[NSFileManager defaultManager] fileExistsAtPath:storePath])
+        {
+            if ([[NSFileManager defaultManager] copyItemAtPath:defaultStorePath toPath:storePath error:&error])
+                NSLog(@"Copied starting data to %@", storePath);
+            else
+                NSLog(@"Error copying default DB to %@ (%@)", storePath, error);
+        }
+        
+        
+        _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+        
+        NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+                                 [NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption,
+                                 [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
+        
+        if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storePathURL options:options error:&error])
+        {
+            
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+        
+        return _persistentStoreCoordinator;
+    }
+}
+
+#pragma mark - Application's Documents directory
+
+// Returns the URL to the application's Documents directory.
+- (NSURL *)applicationDocumentsDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
+#pragma mark - Utility Functions
+
+
+// Builds the starter data file and puts it wherever the simulator stores its data.
+- (void)createStarterDataSet
+{
+    NSManagedObjectContext *context = [[NSManagedObjectContext alloc] init];
+    [context setPersistentStoreCoordinator:self.persistentStoreCoordinator];
+    
+    NSManagedObject *newRule;
+    newRule = [NSEntityDescription insertNewObjectForEntityForName:@"Rules" inManagedObjectContext:context];
+
+    NSError *error;
+    
+    // Default data begins here
+    [newRule setValue: @"Rule 1" forKey:@"text"];
+    
+
+    if (![context save:&error]) {
+        NSLog(@"Error saving: %@", error);
+    }
+
+    NSLog(@"Done initial load");
+    
 }
 
 @end
